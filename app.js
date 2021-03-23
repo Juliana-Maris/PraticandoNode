52222af89b8d4b52eb5d86e61c0e01badb970ccc
José Moreira <canasvieiras@hotmail.com.br>
2021-03-23 16:21:17 -0300

    Atualização

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
 const path = require('path');
 const express = require('express');
 const hbs = require('hbs');
+const bodyParser = require('body-parser')
 const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+app.use(bodyParser.urlencoded({ extended: false}));
 app.use(express.static('public'));
+app.get('/', (req,res) => {
+    res.render('index', {
+        name: "Ju Martinez"
+    });
+});
 
 // rotas
-app.get('/', (req, res) => {
-    res.render('index');
+app.get('/post', (req, res) => {
+    res.render('form');
 });
-app.get('/sobre', (req, res) => {
-    res.send('Esta é a página Sobre')
+app.post('/post', (req, res) => {
+    res.render('index', {
+        name : req.body.textname
+    });
 });
 
 app.listen(8000, () => {
