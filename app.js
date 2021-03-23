9d93d3e256576a5ca25d415f38e7cb001cd2c9fe
José Moreira <canasvieiras@hotmail.com.br>
2021-03-23 11:46:50 -0300

    1 atualização

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
+const path = require('path')
 const express = require('express');
+const handlebars = require('handlebars');
+const { dirname } = require('node:path');
 const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'handlebars');
+app.use(express.static('Publicoboot'));
+app.get('/', (req, res) => {
+    res.render('index');
+})
 
 //rota para home
 app.get('/', (req, res) => {
     res. send('Bem vindo a página inicial');
 });
 // rota pagina sobre
-app.get('/about', (req, res) => {
+app.get('/sobre', (req, res) => {
     res.send('Página: sobre');
 });
 app.listen(8000, () => {
